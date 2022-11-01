1c48ee9e9da25a690d569b790b487cf36c494698
divyanshugarg2017 <divyanshugarg1995@gmail.com>
2022-11-01 21:26:12 +0530

    added image url popup

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -2,23 +2,23 @@ import React from 'react'
 import {MdDeleteOutline} from "react-icons/md"
 import nasa from "../../assets/NASA_logo.svg.png"
 
-const DisplayCard = (props) => {
+const DisplayCard = ({title,url,date,setImageUrl}) => {
+ 
     return (
-        <label htmlFor="my-modal-3" className="card hover:border-2  bg-[#000000] bg-opacity-20 border min-w-[250px] max-w-[320px] h-[375px] cursor-pointer">
+        <label onClick={(e)=>{setImageUrl(url)}} htmlFor="my-modal-3" className="card hover:border-2  bg-[#000000] bg-opacity-20 border min-w-[250px] max-w-[320px] h-[300px] cursor-pointer">
             <div className="grid grid-cols-1">
                 <div class="p-3">
-                    <img className='mx-auto w-full  max-h-[170px]  object-cover' src={nasa} alt="logo" />
-                    <h2 class="card-title py-3 text-white">Bunchinko</h2>
-                    <p className='text-xs text-white flex gap-2 items-center mb-1'>Created On Monday</p>
-                    <p className='text-xs text-white'>chinbonko</p>
-                    <h2 class="card-title text-info text-white pt-3">$ 100</h2>
+                    <img className='mx-auto w-full  max-h-[170px]  object-cover' src={url} alt="logo" />
                 </div>
                 
-                <div className="card-button flex justify-between" onClick={()=>props.setSelectedOrder(props.orderId)}>
-                    <label htmlFor="my-drawer-4" className="btn btn-ghost text-white">
-                        View Details
+                <div className="card-button">
+                    <label htmlFor="my-drawer-4" className="btn btn-ghost w-80 text-white">
+                        {title}
                     </label>
-                    <div className='pr-4 pt-3'><MdDeleteOutline className='text-2xl text-white'/></div>
+                    <div className='flex justify-center'>
+                    <div className='pr-4 pt-3 text-white text-base font-semibold'>{date}</div>
+                    </div>
+                   
                 </div>
             </div>
         </label>
--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -7,81 +7,126 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
 
-
 function Homepage() {
-    const [apiData, setApiData] = useState()
-    const [imageUrl,setImageUrl] = useState()
+    const [apiData, setApiData] = useState([])
+    const [imageUrl, setImageUrl] = useState()
+    const [startDate, setStartDate] = useState("2022-01-01");
+    const [endDate, setEndDate] = useState("2022-01-12");
 
-    const getNasaData = async () => {
-        const response = await axios.get("https://api.nasa.gov/planetary/apod?api_key=gaff4Pwpu0Qg6woyFty1YhVRxhj4In1ImvOCyFD7&start_date=2022-10-01&end_date=2022-10-29&thumbs=true")
-        setApiData(response)
+    const manipulateDate = () => {
+        let dateString = startDate;
+        let myStartDate = new Date(dateString);
+        myStartDate.setDate(myStartDate.getDate() + 7);
+        let date = new Date(myStartDate.toDateString().replace('IST', ''));
+        let day = date.getDate();
+        let month = date.getMonth() + 1;
+        let year = date.getFullYear();
+        myStartDate = year + "-" + month + "-" + day;
+        setStartDate(myStartDate);
+
+        dateString = endDate;
+        let myEndDate = new Date(dateString);
+        myEndDate.setDate(myEndDate.getDate() + 7);
+        date = new Date(myEndDate.toDateString().replace('IST', ''));
+        day = date.getDate();
+        month = date.getMonth() + 1;
+        year = date.getFullYear();
+        myEndDate = year + "-" + month + "-" + day;
+        setEndDate(myEndDate);
+
+    }
+
+    const getNasaData = () => {
+        console.log("call ho jao bahinchod")
+        setTimeout(() => {
+            fetch(`https://api.nasa.gov/planetary/apod?api_key=gaff4Pwpu0Qg6woyFty1YhVRxhj4In1ImvOCyFD7&start_date=${startDate}&end_date=${endDate}&thumbs=true`)
+                .then(response => response.json())
+                .then(json => {
+                    console.log(json);
+                    setApiData([...apiData, ...json]);
+                    manipulateDate();
+                })
+        }, 500);
     }
 
 
     useEffect(() => {
         getNasaData()
+        window.onscroll = function() {
+            if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+             alert("At the bottom!")
+            }
+           }
     }, [])
 
     const displayImage = () => {
-        if (apiData?.data[apiData?.data.length - 1]?.media_type == "image") {
+        if (apiData?.[apiData?.length - 1]?.media_type == "image") {
             return (<label htmlFor="my-modal-3">
-                {apiData ? <img onClick={(e)=>{setImageUrl(apiData?.data[apiData?.data.length - 1]?.url)}} src={apiData?.data[apiData?.data.length - 1]?.url} className="max-w-sm rounded-lg shadow-2xl w-full flex justify-center object-cover   lg:w-[400px] h-[400px] " alt="placeholder" />
+                {apiData ? <img onClick={(e) => { setImageUrl(apiData[apiData?.length - 1]?.url) }} src={apiData[apiData?.length - 1]?.url} className="max-w-sm rounded-lg shadow-2xl w-full flex justify-center object-cover   lg:w-[400px] h-[400px] " alt="placeholder" />
                     : <div className='w-[300px] h-[200px] border'></div>}
             </label>)
         } else {
             return (<div>
-                <source src={apiData?.data[apiData?.data.length - 1]?.url} type="video/mp4" />
+                <source src={apiData[apiData?.length - 1]?.url} type="video/mp4" />
             </div>)
         }
 
     }
 
     return (
-        <div className='min-h-screen bg-[#000000] px-10 pb-20'>
-            <div className="hero h-auto  bg-[#000000] sm:pt-20 lg:pt-20">
-                <div className="hero-content flex-col lg:flex-row-reverse ">
-                    <div>
-                        {displayImage()}
-                    </div>
+        <div>
+
+            <div className='min-h-screen bg-[#000000] px-10 pb-20'>
+                <div className="hero h-auto  bg-[#000000] sm:pt-20 lg:pt-20">
+                    <div className="hero-content flex-col lg:flex-row-reverse ">
+                        <div>
+                            {displayImage()}
+                        </div>
 
 
-                    <div className='pr-10'>
-                        <h1 className='text-5xl text-white font-black pb-20 lg:text-7xl'>Divyanshu Garg</h1>
-                        <h1 className="text-5xl text-white font-bold lg:max-w-[500px]">{apiData?.data[apiData?.data.length - 1].title}</h1>
-                        <p title={apiData?.data[0].explanation} className="py-6 text-white lg:max-w-[500px]" >{apiData?.data[apiData?.data.length - 1].explanation.substring(0, apiData?.data[0].explanation.length / 2)}...</p>
-                        <p className="py-6 text-white font-semibold">Author : {apiData?.data[apiData?.data.length - 1].copyright}</p>
+                        <div className='pr-10'>
+                            <h1 className='text-5xl text-white font-black pb-20 lg:text-7xl'>Divyanshu Garg</h1>
+                            <h1 className="text-5xl text-white font-bold lg:max-w-[500px]">{apiData?.[apiData.length - 1]?.title}</h1>
+                            <p title={apiData?.[apiData.length - 1]?.explanation} className="py-6 text-white lg:max-w-[500px]" >{apiData[apiData?.length - 1]?.explanation.substring(0, apiData[0]?.explanation.length / 2)}...</p>
+                            <p className="py-6 text-white font-semibold">Author : {apiData[apiData.length - 1]?.copyright}</p>
 
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='pt-40  grid grid-cols-1 gap-4 items-center '>
-                <Carousel responsive={responsive} itemClass="carouselItem">
-                    <DisplayCard />
-                    <DisplayCard />
-                    <DisplayCard />
-                    <DisplayCard />
-                    <DisplayCard />
-                    <DisplayCard />
-                    <DisplayCard />
-                </Carousel>
 
-            </div>
-            <div className='pt-40  grid grid-cols-1 gap-4 items-center '>
-                <Carousel responsive={responsive} itemClass="carouselItem">
-                    <DisplayCard />
-                    <DisplayCard />
-                    <DisplayCard />
-                    <DisplayCard />
-                    <DisplayCard />
-                    <DisplayCard />
-                    <DisplayCard />
-                </Carousel>
-            </div>
-            <input type="checkbox" id="my-modal-3" className="modal-toggle" />
-            <div className="modal rounded-lg">
-                <div className="modal-box rounded-lg relative bg-[#000000] ">
-                    <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <img className='' src={imageUrl} alt="overlayImage"/>
+
+                <InfiniteScroll
+                    dataLength={apiData.length}
+                    next={getNasaData}
+                    hasMore={true}
+                    loader={<h4>Loading...</h4>}
+                    scrollThreshold="500px"
+                    pullDownToRefreshThreshold
+                >
+
+                    <div className='pt-40  grid grid-cols-1 gap-4 items-center '>
+                        <Carousel responsive={responsive} itemClass="carouselItem">
+                            {
+                                apiData?.map((item, index) => {
+                                    return (
+                                        <DisplayCard
+                                            setImageUrl={setImageUrl}
+                                            title={item.title}
+                                            url={item.url}
+                                            date={item.date}
+                                        />
+                                    )
+                                })
+                            }
+                        </Carousel>
+                    </div>
+                </InfiniteScroll>
+                <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+                <div className="modal rounded-lg">
+                    <div className="modal-box rounded-lg relative bg-[#000000] ">
+                        <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                        <img className='' src={imageUrl} alt="overlayImage" />
+                    </div>
                 </div>
             </div>
         </div>
